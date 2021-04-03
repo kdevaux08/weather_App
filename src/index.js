7b4f3e62d2f16fcef7c8b9ac7dd69d6d1c90855a
Kayla D <79425718+kdevaux08@users.noreply.github.com>
2021-04-02 19:40:57 -0500

    Added Weather Attributes
    Added the following weather attributes: description, humidity, sunrise/sunset, and wind speed. Updated Javascript to allow the above to update. Updated CSS for the above attributes.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-function formatDate(date) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -8,7 +9,6 @@ function formatDate(date) {
     minutes = `0${minutes}`;
   }
 
-  let dayIndex = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -18,11 +18,29 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  let day = days[dayIndex];
-
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Thurs", "Fri", "Sat"];
+  return days[day];
+}
 
+function showUpdate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
 let dateElement = document.querySelector("#current");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
@@ -32,17 +50,25 @@ function displayWeatherCondition(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
+  document.querySelector("#weatherDescription").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#sunrise").innerHTML = showUpdate(
+    response.data.sys.sunrise * 1000
+  );
+  document.querySelector("#sunset").innerHTML = showUpdate(
+    response.data.sys.sunset * 1000
+  );
+  document.querySelector("#weatherHumidity").innerHTML =
+    response.data.main.humidity;
+  document.querySelector("#weatherWind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
 }
 
 function searchCity(city) {
   let apiKey = "fc74017ad8bc17cad484f8ec81c6d2c4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherCondition);
-}
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  searchCity(city);
 }
 
 function searchLocation(position) {
@@ -55,6 +81,12 @@ function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
 let searchForm = document.querySelector("#city-form");
 searchForm.addEventListener("submit", handleSubmit);
 
